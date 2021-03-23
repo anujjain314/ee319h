8b8076fee002b51af20d67c43e457211e264f15f
anujjain314 <anujjain314@utexas.edu>
2021-03-22 19:16:37 -0500

    Key_init and Key_In complete

--- Lab6_EE319K/Key.c
+++ Lab6_EE319K/Key.c
@@ -2,7 +2,7 @@
 // This software configures the off-board piano keys
 // Lab 6 requires a minimum of 3 keys, but you could have more
 // Runs on TM4C123
-// Program written by: put your names here
+// Program written by: Anuj Jain and George Koussa
 // Date Created: 3/6/17 
 // Last Modified: 1/17/21  
 // Lab number: 6
@@ -15,12 +15,15 @@
 #include "../inc/tm4c123gh6pm.h"
 
 // **************Key_Init*********************
-// Initialize piano key inputs on PA5-2 PB3-0 or PE3-0
+// Initialize piano key inputs on PE3-0
 // Input: none 
 // Output: none
 void Key_Init(void){ 
-  // write this
-
+  SYSCTL_RCGCGPIO_R |= 0x10;
+	volatile int a = 4;
+	a++;
+	GPIO_PORTE_DIR_R |= 0x0F;
+	GPIO_PORTE_DIR_R |= 0x0F;
 }
 // **************Key_In*********************
 // Input from piano key inputs on PA5-2 PB3-0 or PE3-0
@@ -28,7 +31,10 @@ void Key_Init(void){
 // Output: 0 to 15 depending on keys
 //   0x01 is just Key0, 0x02 is just Key1, 0x04 is just Key2, 0x08 is just Key3
 uint32_t Key_In(void){ 
-  // write this
- return 42;
+	uint8_t keyData;
+	GPIO_PORTE_DATA_R &= 0x0F;
+	keyData = (GPIO_PORTE_DATA_R) & (0x0F);
+	
+	return keyData;
 }
 
