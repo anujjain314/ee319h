8bba4522e487163f449f8b2af50af3911637ddf7
George Koussa <georgekoussa@utexas.edu>
2021-04-11 22:07:01 -0500

    completed ADC driver

--- Lab8_C++/SlidePot.cpp
+++ Lab8_C++/SlidePot.cpp
@@ -15,7 +15,25 @@
 // Output: none
 // measures from PD2, analog channel 5
 void ADC_Init(uint32_t sac){ 
-//*** students write this ******
+  SYSCTL_RCGCADC_R |= 0x0001;   // 1) activate ADC0
+  SYSCTL_RCGCGPIO_R |= 0x08;    // 2) activate clock for Port E
+  while((SYSCTL_PRGPIO_R&0x10) != 0x10){};  // 3 for stabilization
+  GPIO_PORTE_DIR_R &= ~0x08;    // 4) make PE4 input
+  GPIO_PORTE_AFSEL_R |= 0x08;   // 5) enable alternate function on PE4
+  GPIO_PORTE_DEN_R &= ~0x08;    // 6) disable digital I/O on PE4
+  GPIO_PORTE_AMSEL_R |= 0x08;   // 7) enable analog functionality on PE4
+// while((SYSCTL_PRADC_R&0x0001) != 0x0001){}; // good code, but not implemented in simulator
+  ADC0_PC_R &= ~0xF;
+  ADC0_PC_R |= 0x1;             // 8) configure for 125K samples/sec
+  ADC0_SSPRI_R = 0x0123;        // 9) Sequencer 3 is highest priority
+  ADC0_ACTSS_R &= ~0x0008;      // 10) disable sample sequencer 3
+  ADC0_EMUX_R &= ~0xF000;       // 11) seq3 is software trigger
+  ADC0_SSMUX3_R &= ~0x000F;
+  ADC0_SSMUX3_R += 9;           // 12) set channel
+  ADC0_SSCTL3_R = 0x0006;       // 13) no TS0 D0, yes IE0 END0
+  ADC0_IM_R &= ~0x0008;         // 14) disable SS3 interrupts
+  ADC0_ACTSS_R |= 0x0008;       // 15) enable sample sequencer 3
+	ADC0_SAC_R = sac;
 }
 
 //------------ADCIn------------
@@ -24,8 +42,12 @@ void ADC_Init(uint32_t sac){
 // Output: 12-bit result of ADC conversion
 // measures from PD2, analog channel 5
 uint32_t ADC_In(void){  
-	//*** students write this ******
-  return 42; // remove this, replace with real code
+	uint32_t result;
+  ADC0_PSSI_R = 0x0008;
+    while((ADC0_RIS_R&0x08) == 0);
+    result = ADC0_SSFIFO3_R&0xFFF;
+    ADC0_ISC_R = 0x0008;
+    return result;
 }
 
 // constructor, invoked on creation of class
